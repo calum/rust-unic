b0c8b542f0c88066bc4ea78ab88fc0045333f212
Calum Forster <calumforster.play@gmail.com>
2017-07-30 18:57:34 +0100

    Removed unnecessary comments and code

--- unic/ucd/tests/category_tests.rs
+++ unic/ucd/tests/category_tests.rs
@@ -16,7 +16,7 @@ extern crate unic_ucd;
 extern crate unic_utils;
 
 
-use unic_ucd::bidi::BidiClass;
+use unic_ucd::bidi::BidiClass as BC;
 use unic_ucd::normal::is_combining_mark;
 use unic_ucd::category::GeneralCategory as GC;
 use unic_utils::iter_all_chars;
@@ -26,39 +26,25 @@ use unic_utils::iter_all_chars;
 #[test]
 fn test_gen_cat_against_normal() {
     for cp in iter_all_chars() {
-        // Every General_Category mark must be a combining mark
-        if GC::of(cp).is_mark() {
-            assert!(is_combining_mark(cp));
-        }
-
-        // Every combining mark must be a General_Category mark
-        if is_combining_mark(cp) {
-            assert!(GC::of(cp).is_mark());
-        }
+        assert_eq!(GC::of(cp).is_mark(), is_combining_mark(cp));
     }
 }
 
-/// `Bidi_Class=EN := General_Category in { N (Number) }`
-///
 /// <http://www.unicode.org/reports/tr9/#EN>
 #[test]
 fn test_bidi_en_against_gen_cat() {
-    // Every BC::EuropeanNumber must satisfy GC::is_number()
     for cp in iter_all_chars() {
-        if BidiClass::of(cp) == BidiClass::EuropeanNumber {
+        if BC::of(cp) == BC::EuropeanNumber {
             assert!(GC::of(cp).is_number());
         }
     }
 }
 
-/// `Bidi_Class=ES := General_Category in { Sm (MathSymbol), Pd (DashPunctuation) }`
-///
 /// <http://www.unicode.org/reports/tr9/#ES>
 #[test]
 fn test_bidi_es_against_gen_cat() {
-    // Every BC::EuropeanSeparator must be a GC::MathSymbol, GC::DashPunctuation
     for cp in iter_all_chars() {
-        if BidiClass::of(cp) == BidiClass::EuropeanSeparator {
+        if BC::of(cp) == BC::EuropeanSeparator {
             assert!(
                 GC::of(cp) == GC::MathSymbol ||
                 GC::of(cp) == GC::DashPunctuation
@@ -67,16 +53,11 @@ fn test_bidi_es_against_gen_cat() {
     }
 }
 
-/// `Bidi_Class=ET := General_Category in { S (Symbol), Cn (Unassigned),
-///                                         Po (OtherPunctuation) }`
-///
 /// <http://www.unicode.org/reports/tr9/#ET>
 #[test]
 fn test_bidi_et_against_gen_cat() {
-    // Every BC::EuropeanTerminator must satisfy GC::is_symbol() or
-    // be GC::Unassigned or GC::OtherPunctuation
     for cp in iter_all_chars() {
-        if BidiClass::of(cp) == BidiClass::EuropeanTerminator {
+        if BC::of(cp) == BC::EuropeanTerminator {
             assert!(
                 GC::of(cp).is_symbol()          ||
                 GC::of(cp) == GC::Unassigned    ||
@@ -86,16 +67,11 @@ fn test_bidi_et_against_gen_cat() {
     }
 }
 
-/// `Bidi_Class=AN := General_Category in { Cf (Format), No (OtherNumber),
-///                                         Po (OtherPunctuation), Nd (DecimalNumber) }`
-///
 /// <http://www.unicode.org/reports/tr9/#AN>
 #[test]
 fn test_bidi_an_against_gen_cat() {
-    // Every BC::ArabicNumber must be a GC::Format, GC::OtherNumber, GC::OtherPunctuation,
-    // or GC::DecimalNumber
     for cp in iter_all_chars() {
-        if BidiClass::of(cp) == BidiClass::ArabicNumber {
+        if BC::of(cp) == BC::ArabicNumber {
             assert!(
                 GC::of(cp) == GC::Format            ||
                 GC::of(cp) == GC::OtherNumber       ||
@@ -106,16 +82,11 @@ fn test_bidi_an_against_gen_cat() {
     }
 }
 
-/// `Bidi_Class=CS := General_Category in { Po (OtherPunctuation), Zs (SpaceSeparator),
-///                                         Sm (MathSymbol) }`
-///
 /// <http://www.unicode.org/reports/tr9/#CS>
 #[test]
 fn test_bidi_cs_against_gen_cat() {
-    // Every BC::CommonSeparator must be a GC::OtherPunctuation, GC::SpaceSeparator,
-    // or GC::MathSymbol
     for cp in iter_all_chars() {
-        if BidiClass::of(cp) == BidiClass::CommonSeparator {
+        if BC::of(cp) == BC::CommonSeparator {
             assert!(
                 GC::of(cp) == GC::OtherPunctuation  ||
                 GC::of(cp) == GC::SpaceSeparator    ||
@@ -132,7 +103,7 @@ fn test_bidi_cs_against_gen_cat() {
 fn test_bidi_nsm_against_gen_cat() {
     // Every NSM must be a GC=Mark
     for cp in iter_all_chars() {
-        if BidiClass::of(cp) == BidiClass::NonspacingMark {
+        if BC::of(cp) == BC::NonspacingMark {
             assert!(is_combining_mark(cp));
         }
     }
@@ -140,37 +111,26 @@ fn test_bidi_nsm_against_gen_cat() {
     // Every GC!=Mark must not be an NSM
     for cp in iter_all_chars() {
         if !is_combining_mark(cp) {
-            assert_ne!(BidiClass::of(cp), BidiClass::NonspacingMark);
+            assert_ne!(BC::of(cp), BC::NonspacingMark);
         }
     }
 }
 
-/// `Bidi_Class=BN := General_Category in { Cc (Control), Co (PrivateUse) }`
-///
 /// <http://www.unicode.org/reports/tr9/#BN>
 #[test]
 fn test_bidi_bn_against_gen_cat() {
     for cp in iter_all_chars() {
-        // Every BoundaryNeutral must satisfy GC::is_other()
-        if BidiClass::of(cp) == BidiClass::BoundaryNeutral {
+        if BC::of(cp) == BC::BoundaryNeutral {
             assert!(GC::of(cp).is_other());
-        }
-
-        // Every !GC::is_other() must not be an BoundaryNeutral
-        if !GC::of(cp).is_other() {
-            assert_ne!(BidiClass::of(cp), BidiClass::BoundaryNeutral);
         }
     }
 }
 
-/// `Bidi_Class=B := General_Category in { Cc (Control), Zp (ParagraphSeparator) }`
-///
 /// <http://www.unicode.org/reports/tr9/#B>
 #[test]
 fn test_bidi_b_against_gen_cat() {
-    // Every ParagraphSeparator must be a GC::Control or GC::ParagraphSeparator
     for cp in iter_all_chars() {
-        if BidiClass::of(cp) == BidiClass::ParagraphSeparator {
+        if BC::of(cp) == BC::ParagraphSeparator {
             assert!(
                 GC::of(cp) == GC::Control ||
                 GC::of(cp) == GC::ParagraphSeparator
@@ -179,27 +139,21 @@ fn test_bidi_b_against_gen_cat() {
     }
 }
 
-/// `Bidi_Class=S := General_Category in { Cc (Control) }`
-///
 /// <http://www.unicode.org/reports/tr9/#S>
 #[test]
 fn test_bidi_s_against_gen_cat() {
-    // Every Segment Separator must be a GC::Control
     for cp in iter_all_chars() {
-        if BidiClass::of(cp) == BidiClass::SegmentSeparator {
+        if BC::of(cp) == BC::SegmentSeparator {
             assert!(GC::of(cp) == GC::Control);
         }
     }
 }
 
-/// `Bidi_Class=WS := General_Category in { Cc (Control), Zs (SpaceSeparator), Zl (LineSeparator) }`
-///
 /// <http://www.unicode.org/reports/tr9/#WS>
 #[test]
 fn test_bidi_ws_against_gen_cat() {
-    // Every BC::WhiteSpace must be a GC::Control, GC::SpaceSeparator, or GC::LineSeparator
     for cp in iter_all_chars() {
-        if BidiClass::of(cp) == BidiClass::WhiteSpace {
+        if BC::of(cp) == BC::WhiteSpace {
             assert!(
                 GC::of(cp) == GC::Control           ||
                 GC::of(cp) == GC::SpaceSeparator    ||
